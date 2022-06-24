8bcc0b9ca02ca7cbe047643780f84806348ee3d9
Camila Ramos <camiinthisthang@Camilas-MacBook-Pro-3.local>
2022-06-24 15:54:42 -0400

    added import

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import connectContract from "../utils/connectContract";
 
 const Home: NextPage = () => {
   return (
